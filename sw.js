b5ba4b8387d2db7779249d6e4132fcd392b83433
Adriano Monteiro <adri0mt@gmail.com>
2026-07-20 21:10:00 -0300

    Versão 1.0.0 -  Calendário de escala com PWA

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'calendario-escalas-v1';
+const urlsToCache = [
+    '/calendario-escalas/',
+    '/calendario-escalas/index.html',
+    '/calendario-escalas/manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache aberto');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
